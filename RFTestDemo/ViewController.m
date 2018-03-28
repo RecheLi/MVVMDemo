0471cceeb664a9c9625f666123ccb3d212198885
linitial <linitial@linitialdeMacBook-Pro.local>
2018-03-28 13:41:55 +0800

    Initial Commit

---
+++ RFTestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RFTestDemo
+//
+//  Created by linitial on 2018/3/28.
+//  Copyright © 2018年 redfinge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
